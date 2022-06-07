a4d9a950f17695e5e2e1401de93eba234e821b2e
peppino708 <708.baseball.ster@gmail.com>
2022-06-07 16:14:50 +0900

    set name by button click

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -87,11 +87,12 @@ export const SignIn: FC<Props> = (props) => {
               onChange={(e) => setString(e.target.value)}
             />
             <Button
-              type="submit"
+              type="button"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
               disabled={disabled}
+              onClick={() => setName(string)}
             >
               はじめる
             </Button>
